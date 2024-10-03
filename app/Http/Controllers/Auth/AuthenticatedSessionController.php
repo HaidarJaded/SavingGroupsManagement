47559dbcf2f29a622e9b64b71f3ebd26f0e33769
Haidar Jaded <haidar.king787@gmail.com>
2024-10-03 13:04:07 +0300

    Delete unnecessary files

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -3,13 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Requests\Auth\LoginRequest;
-use App\Http\Requests\Auth\ChangePasswordRequest;
 use function auth;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Validation\ValidationException;
-use Illuminate\Support\Facades\Hash;
 
 /**
  * @group Sessions management
