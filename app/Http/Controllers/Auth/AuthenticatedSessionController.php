a3de2bbeea14dc33c730e6e36103b391660b0621
Haidar Jaded <haidar.king787@gmail.com>
2024-10-03 04:36:43 +0300

    Remove api config from project

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -3,9 +3,8 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Requests\Auth\LoginRequest;
-use App\Http\Requests\ChangePasswordRequest;
+use App\Http\Requests\Auth\ChangePasswordRequest;
 use function auth;
-use App\Traits\ApiResponseTrait;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -21,7 +20,6 @@ class AuthenticatedSessionController extends Controller
      * Handle an incoming authentication request.
      * @throws ValidationException
      */
-    use ApiResponseTrait;
 
     #function to restore login page
     public function showLoginPage()
@@ -42,30 +40,7 @@ class AuthenticatedSessionController extends Controller
             'email' => 'The provided credentials do not match our records.',
         ])->withInput($loginRequest->only('email'));
     }
-    /**
-     * Login
-     * @param LoginRequest $request
-     * @return JsonResponse
-     * @throws ValidationException
-     * @unauthenticated
-     */
-    public function store(LoginRequest $request): JsonResponse
-    {
-        $request->authenticate();
 
-        $user = auth()->user();
-
-        // $user->load('rule', 'permissions', 'rule.permissions');
-        $expiration = config('sanctum.expiration');
-        $expires_at = now()->addMinutes($expiration);
-        $token = $user->createToken('login', ['*'], $expires_at);
-
-        $message = [
-            'auth' => $user,
-            'token' => $token->plainTextToken
-        ];
-        return $this->apiResponse($message);
-    }
 
     public function logout()
     {
@@ -73,34 +48,5 @@ class AuthenticatedSessionController extends Controller
         session()->invalidate();
         session()->regenerateToken();
         return redirect('/login')->with('message', 'You have been logged out.');
-    }
-
-    /**
-     * Refresh token
-     * @param Request $request
-     * @return JsonResponse
-     */
-    public function refresh_token(Request $request): JsonResponse
-    {
-        $user = $request->user();
-        $oldTokenId = $request->user()->currentAccessToken()->id;
-        $expiration = config('sanctum.expiration');
-        $expires_at = now()->addMinutes($expiration);
-        $token = $request->user()->createToken('refresh-token', ['*'], $expires_at);
-        $user->tokens()->where('id', $oldTokenId)->delete();
-        return $this->apiResponse(['token' => $token->plainTextToken]);
-    }
-
-    public function changePassword(ChangePasswordRequest $request): JsonResponse
-    {
-        $user = $request->user();
-        if (!Hash::check($request->input('current_password'), $user->password)) {
-            return $this->apiResponse(['Error Current password does not match'], 422, 'كلمة المرور الحالية غير صحيحة');
-        }
-
-        $user->password = Hash::make($request->input('new_password'));
-        $user->save();
-
-        return $this->apiResponse([], 200, 'Password changed successfully');
     }
 }
