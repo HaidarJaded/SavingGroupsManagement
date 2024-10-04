2a2cef9604d274c217581490c74f2eb28a362e65
Haidar Jaded <haidar.king787@gmail.com>
2024-10-05 01:57:59 +0300

    Add routes for payments and transactions

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,12 @@ use App\Http\Controllers\TransactionController;
 Route::get('login', [AuthenticatedSessionController::class, 'showLoginPage'])->name('login');
 Route::post('login', [AuthenticatedSessionController::class, 'login']);
 Route::middleware('auth')->group(function () {
-    Route::resource('transactions',TransactionController::class);
     Route::resource('saving_groups',SavingGroupController::class);
+    Route::get('transactions/{saving_group_id}',[TransactionController::class,'create'])->name('transactions.create');
+    Route::get('transactions',[TransactionController::class,'index'])->name('transactions.index');
+    Route::post('transactions',[TransactionController::class,'store'])->name('transactions.store');
+    Route::delete('transactions/destroy/{transaction_id}',[TransactionController::class,'destroy'])->name('transactions.destroy');
+    Route::resource('payments',PaymentController::class);
     Route::get('saving_group_subscribers/{id}',[SavingGroupController::class,'saving_group_subscribers'])->name('saving_group_subscribers');
     Route::get('payments/subscriber/{id}',[SubscriberController::class,'show_payments'])->name('payments.subscriber');
     Route::post('/payments', [PaymentController::class, 'store'])->name('payments.store');
