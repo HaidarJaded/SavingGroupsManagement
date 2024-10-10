1ba4f763f154b6392a0c3c725023f84446535752
Haidar Jaded <haidar.king787@gmail.com>
2024-10-11 00:35:37 +0300

    Modify on cron job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('increment:day-cycle')->runInBackground();
+        $schedule->command('increment:day-cycle')->daily();
     }
 
     /**
